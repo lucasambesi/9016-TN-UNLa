0df851002a1a3ae2097353845682af8fd8e1bedc
Lucas Ambesi <ambesilucas@gmail.com>
2022-09-04 17:27:36 -0300

    add delay splash screen

--- GOT-App/app/src/main/java/com/example/got_app/SplashScreenActivity.java
+++ GOT-App/app/src/main/java/com/example/got_app/SplashScreenActivity.java
@@ -2,13 +2,26 @@ package com.example.got_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final long tiempo_espera = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, tiempo_espera);
     }
 }
