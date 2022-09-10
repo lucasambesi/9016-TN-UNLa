7f0d94c59646cefc3212ad50c692f7ded4192c95
Lucas Ambesi <ambesilucas@gmail.com>
2022-09-10 14:23:04 -0300

    Add Constantes

---
+++ GOT-App/app/src/main/java/com/example/got_app/Constantes.java
@@ -0,0 +1,7 @@
+package com.example.got_app;
+
+public class Constantes {
+    public static final String SP_CREDENCIALES = "sp_credenciales";
+    public static final String USUARIO  = "usuario";
+    public static final String PASSWORD  = "password";
+}
