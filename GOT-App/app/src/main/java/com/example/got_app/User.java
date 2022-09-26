2aacd8c4a91d7a24fd19c2a63cd96bfd926b94a0
Lucas Ambesi <ambesilucas@gmail.com>
2022-09-26 18:14:36 -0300

    Add orm user class

--- GOT-App/app/src/main/java/com/example/got_app/User.java
+++ GOT-App/app/src/main/java/com/example/got_app/User.java
@@ -1,8 +1,18 @@
 package com.example.got_app;
 
+import com.j256.ormlite.field.DatabaseField;
+import com.j256.ormlite.table.DatabaseTable;
+
+@DatabaseTable(tableName = "users")
 public class User {
+
+    @DatabaseField(id = true)
     private Integer id;
+
+    @DatabaseField
     private String name;
+
+    @DatabaseField
     private String password;
 
     public User() {
