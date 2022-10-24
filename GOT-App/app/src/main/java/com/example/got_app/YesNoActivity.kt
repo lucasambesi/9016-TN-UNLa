ec464db6b7fd506016e5d4296e4767c8a1da6a66
Lucas Ambesi <ambesilucas@gmail.com>
2022-10-24 16:46:36 -0300

    rename activity yesno

--- GOT-App/app/src/main/java/com/example/got_app/YesNoActivity.kt
+++ GOT-App/app/src/main/java/com/example/got_app/YesNoActivity.kt
@@ -1,6 +1,5 @@
 package com.example.got_app
 
-import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -9,26 +8,21 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.Button
-import android.widget.CheckBox
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.widget.Toolbar
-import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-import com.squareup.picasso.Picasso
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import retrofit2.Call
-import retrofit2.Response
 
-class MainActivity3 : AppCompatActivity() {
+class YesNoActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
 
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main3)
+        setContentView(R.layout.activity_yesno)
         val toolbar = findViewById<View>(R.id.tbYesNo) as Toolbar
         toolbar.title = "Yes/No"
         setSupportActionBar(toolbar)
@@ -45,7 +39,7 @@ class MainActivity3 : AppCompatActivity() {
                     Log.d("YesNo: ", yesno.toString())
                     if (yesno != null) {
                         Log.d("YesNo image: ", yesno.image.toString())
-                        Glide.with(this@MainActivity3)
+                        Glide.with(this@YesNoActivity)
                             .load(yesno.image.toString())
                             .into(ivYesNo)
                         Thread.sleep(6500)
@@ -62,14 +56,12 @@ class MainActivity3 : AppCompatActivity() {
         btnRecargar.setOnClickListener(
             object : View.OnClickListener {
                 override fun onClick(v: View) {
-                    val main_activity = Intent(this@MainActivity3, MainActivity3::class.java)
+                    val main_activity = Intent(this@YesNoActivity, YesNoActivity::class.java)
                     startActivity(main_activity)
                     finish()
                 }
             })
     }
-
-
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_house, menu)
@@ -79,7 +71,7 @@ class MainActivity3 : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             R.id.iBack -> {
-                val main_activity = Intent(this@MainActivity3, MainActivity::class.java)
+                val main_activity = Intent(this@YesNoActivity, MainActivity::class.java)
                 startActivity(main_activity)
                 true
             }
